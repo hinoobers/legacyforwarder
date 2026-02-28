770d8d12357d65dd85aea3918240d7903042c48b
hinoob <reiopoldmaaa113@gmail.com>
2026-02-28 03:17:47 +0200

    Get base working / add all dependencies

---
+++ src/main/java/org/hinoob/legacyforwarder/LegacyForwarder.java
@@ -0,0 +1,19 @@
+package org.hinoob.legacyforwarder;
+
+import com.github.retrooper.packetevents.PacketEvents;
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LegacyForwarder extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        PacketEvents.getAPI().load();
+    }
+
+    @Override
+    public void onEnable() {
+        PacketEvents.getAPI().init();
+    }
+}
